e1e37dff0f14e08ec6cdf123dbca55ac8221d384
Taiki Endo <te316e89@gmail.com>
2019-08-16 21:42:49 +0900

    Use trybuild instead of compiletest

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,4 +1,3 @@
-#![cfg(compiletest)]
 #![cfg(all(
     feature = "std",
     feature = "type_analysis",
@@ -8,31 +7,10 @@
     feature = "read_initializer",
 ))]
 
-use std::{env, path::PathBuf};
-
-fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
-    config.mode = mode.parse().expect("invalid mode");
-    let mut me = env::current_exe().unwrap();
-    me.pop();
-    config.target_rustcflags = Some(format!(
-        "--edition=2018 \
-         -Z unstable-options \
-         --extern auto_enums \
-         -L {}",
-        me.display()
-    ));
-    let src = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    config.src_base = src.join("tests").join(mode);
-
-    me.pop();
-    me.pop();
-    config.build_base = me.join("tests").join(mode);
-    compiletest::run_tests(&config);
-}
-
 #[rustversion::attr(not(nightly), ignore)]
 #[test]
-fn compiletest() {
-    run_mode("ui");
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/auto_enum/*.rs");
+    t.compile_fail("tests/ui/enum_derive/*.rs");
 }
